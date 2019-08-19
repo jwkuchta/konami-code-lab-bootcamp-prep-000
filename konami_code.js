489fda068a6e72fec276177ac3810b1647c25d81
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-19 23:47:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,22 +12,19 @@ const codes = [
 ];
 
 function init() {
-  //
-  var index = 0
-  document.body.addEventListener('keydown', onKeyDownHandler);
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
 
-   function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which)
-    console.log('key', key)
-    if (key === code[index]) {
-      index++
+     if (key === code[index]) {
+      index++;
 
-       if (index === code.length) {
-        alert("YOU DID IT!")
-        index = 0
+       if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
       }
     } else {
-      index = 0
+      index = 0;
     }
-  }
+  }, false);
 }
