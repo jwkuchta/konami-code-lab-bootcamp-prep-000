7909d3ed0f9767320aa01cd806982f32999212ae
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-19 23:53:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,23 +12,24 @@ const codes = [
 ];
 
 function init() {
-  let index = 0;
+  if (window.addEventListener) {
+      // create the keys and konami variables
+      var keys = [],
+          konami = "38,38,40,40,37,39,37,39,66,65";
 
-document.body.addEventListener("keydown", (thingPressed) => {
-  const key = thingPressed.key
-  console.log(key)
+      // bind the keydown event to the Konami function
+      window.addEventListener("keydown", function(e){
+          // push the keycode to the 'keys' array
+          keys.push(e.keyCode);
 
-  if (codes[index] == key) {
-    ++index
+          // and check to see if the user has entered the Konami code
+          if (keys.toString().indexOf(konami) >= 0) {
+              // do something such as:
+              alert("you did it!")
+
+              // and finally clean up the keys array
+              keys = [];
+          };
+      }, true);
   }
-
-  else {
-    idx = 0
-  }
-
-  console.log(index)
-  if (index == 10) {
-    alert("Hurray!");
-    index = 0
-  }
-}); 
+}
